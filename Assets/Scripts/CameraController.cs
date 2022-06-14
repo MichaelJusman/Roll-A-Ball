6154b0682d224effbc3edaa531a65b957837a8e9
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2022-06-15 09:38:35 +1000

    Camera Follow Functionality
    The camera follow the player by an offset

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset; 
+
+    void Start()
+    {
+        //Set the offset to the cameras position minus the player
+        offset = transform.position - player.transform.position;
+    }
+
+
+    void LateUpdate()
+    {
+        //Set the transform [osition of the camera to that of the player
+        transform.position = player.transform.position;
+
+    }
+}
